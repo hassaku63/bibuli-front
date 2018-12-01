45d5896423d9fe4c28cc5a355691e45377541a76
kazuya.kato <kazuya.kato@serverworks.co.jp>
2018-12-02 02:41:57 +0900

    change appearance of contents field and add footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <v-app>
     <NavBar></NavBar>
-
     <v-content>
       <HelloWorld/>
     </v-content>
+    <v-footer>&copy; 2018- bibuli.</v-footer>
   </v-app>
 </template>
 
@@ -25,3 +25,12 @@ export default {
   }
 }
 </script>
+
+<style>
+.v-content, .v-footer { background-color: #EEEEEE !important; }
+.v-footer {
+  padding: 0 24px;
+  color: #707070!important;
+  font-size: .75rem;
+}
+</style>
