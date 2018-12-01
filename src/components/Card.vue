3a546975f59ae84b1ca0d525f5f1213b1eeffc1c
kazuya.kato <kazuya.kato@serverworks.co.jp>
2018-12-02 07:11:21 +0900

    complete implementation of data-bind

---
+++ src/components/Card.vue
@@ -0,0 +1,12 @@
+<template>
+<ul>
+<li>{{book.title}}</li>
+<li>{{book.author}}</li>
+</ul>
+</template>
+
+<script>
+export default {
+  props: ['book']
+}
+</script>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -15,24 +15,49 @@
         </v-flex>
       <v-flex xs8 sm7 md6>
         <v-text-field
+          v-model="text"
           label="Solo"
           placeholder="search..."
           solo
         ></v-text-field>
       </v-flex>
+    <v-btn flat icon @click="onSearchClicked">
       <v-icon>search</v-icon>
+    </v-btn>
     </v-layout>
+    <Card v-for="(book) in books" :key="book.book_id" :book="book"></Card>
   </v-container>
 </template>
 
 <script>
+import Card from '../components/Card.vue'
+import searcher from '../services/search'
+
 export default {
+  components: {
+    Card
+  },
   data: () => ({
-    items: ['タイトル', '著者']
-  })
+    items: ['タイトル', '著者'],
+    books: [],
+    text: '',
+    error: ''
+  }),
+  methods: {
+    onSearchClicked: function () {
+      let self = this
+      searcher.bookSearch(this.text, 'title')
+        .then(function (books) {
+          books.forEach(book => {
+            self.books.splice(0, self.books.length, ...books)
+          })
+        }).catch(function (e) {
+          console.log(e.message)
+        })
+    }
+  }
 }
 </script>
 
 <style>
-
 </style>
