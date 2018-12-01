c969548f5d2d29658faf9b0ee304bec519ffeafe
kazuya.kato <kazuya.kato@serverworks.co.jp>
2018-12-02 02:30:59 +0900

    change appearance of header

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,16 +1,25 @@
 <template>
-  <v-toolbar app>
-    <v-toolbar-title class="headline text-uppercase">
-      <span>Vuetify</span>
-      <span class="font-weight-light">MATERIAL DESIGN</span>
+  <v-toolbar app flat>
+    <v-toolbar-title class="headline">
+      <router-link to="/" class="logo">bibuli</router-link>
     </v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-btn
-      flat
-      href="https://github.com/vuetifyjs/vuetify/releases/latest"
-      target="_blank"
-    >
-      <span class="mr-2">Latest Release</span>
-    </v-btn>
+    <router-link to="/search">検索</router-link>
   </v-toolbar>
 </template>
+
+<script>
+</script>
+
+<style>
+.v-toolbar {
+  background-color: #4CB5AB !important;
+}
+a {
+  color: #FFF !important;
+  text-decoration: none;
+}
+a.router-link-exact-active:not(.logo) {
+  border-bottom: 4px solid #FFF;
+}
+</style>
