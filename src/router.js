2f62c5b83fcafdf37f502250ec04eb8d506b769f
kkato <kazuya.kato@serverworks.co.jp>
2019-01-29 07:46:47 +0900

    add authentication to router.js

--- src/router.js
+++ src/router.js
@@ -6,33 +6,50 @@ import Login from './views/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+var Auth = {
+  loggedIn: false
+}
+
+var routes = [
+  {
+    path: '/',
+    redirect: '/search'
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: Search
+  },
+  {
+    path: '/mypage',
+    name: 'mypage',
+    component: Mypage
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login,
+    meta: { isPublic: true }
+  },
+  {
+    path: '/logout',
+    name: 'logout',
+    redirect: '/login'
+  }
+]
+
+var router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      redirect: '/search'
-    },
-    {
-      path: '/search',
-      name: 'search',
-      component: Search
-    },
-    {
-      path: '/mypage',
-      name: 'mypage',
-      component: Mypage
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/logout',
-      name: 'logout',
-      redirect: '/login'
-    }
-  ]
+  routes: routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => !record.meta.isPublic) && !Auth.loggedIn) {
+    next({ path: '/login' })
+  } else {
+    next()
+  }
+})
+
+export default router
