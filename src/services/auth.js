f762fb60f3b756680e47c14a52d7490d2d68b515
hassaku63 <takuyahashimoto1988@gmail.com>
2019-04-02 22:01:55 +0900

    [feat] login/logout

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,4 +1,3 @@
-
 import AWS from 'aws-sdk'
 import {
   AuthenticationDetails,
@@ -17,61 +16,104 @@ const loginsKey = 'cognito-idp.' + appConfig.region + '.amazonaws.com/' + appCon
 var cognitoIdentity = {
   userPool: null,
   user: null,
-  token: null
+  token: null,
+  session: null
+}
+
+const getCurrentUserSession = function () { // eslint-disable-line
+  const keyPrefix = 'CognitoIdentityServiceProvider.' + appConfig.ClientId
+  const username = sessionStorage.getItem(keyPrefix + '.LastAuthUser')
+  console.log(keyPrefix + '.' + username)
+  return {
+    'username': sessionStorage.getItem(keyPrefix + '.' + username),
+    'idToken': sessionStorage.getItem(keyPrefix + '.' + username + '.idToken'),
+    'refreshToken': sessionStorage.getItem(keyPrefix + '.' + username + '.refreshToken')
+  }
+}
+
+const refreshAuth = function () { // eslint-disable-line
+  /*
+    API Gatewayへのリクエストを伴う処理を実装する場合は必ずrefreshAuth.thenのコンテキストで実行させる
+    IdTokenがExpireしていた場合にのみリフレッシュを行う
+
+    https://github.com/aws-amplify/amplify-js/tree/master/packages/amazon-cognito-identity-js
+    Use case 32. 参照
+  */
+  return new Promise((resolve, reject) => {
+    if (AWS.config.credentials.needsRefresh()) {
+      // Token refresh
+      var refreshToken = cognitoIdentity.session.getRefreshToken()
+      cognitoIdentity.user.refreshSession(refreshToken, (err, session) => {
+        if (err) {
+          console.error(err)
+          reject(new Error('Refresh session failure'))
+        } else {
+          cognitoIdentity.session = session
+
+          AWS.config.credentials.params.Logins[loginsKey] = session.getIdToken().getJwtToken()
+          AWS.config.credentials.refresh((err) => {
+            if (err) {
+              console.error(err)
+              reject(new Error('Credentials refresh failure'))
+            } else {
+              resolve(getCurrentUserSession())
+            }
+          })
+        }
+      })
+    } else {
+      // No refresh
+      resolve()
+    }
+  })
 }
 
 export default {
   login: function (username, password) {
     return new Promise((resolve, reject) => {
       // try login
-      console.log('new CognitoUserPool')
       cognitoIdentity.userPool = new CognitoUserPool({
         UserPoolId: appConfig.UserPoolId,
-        ClientId: appConfig.ClientId
+        ClientId: appConfig.ClientId,
+        Storage: sessionStorage
       })
 
-      console.log('new CognitoUser')
       cognitoIdentity.user = new CognitoUser({
         Username: username,
-        Pool: cognitoIdentity.userPool
+        Pool: cognitoIdentity.userPool,
+        Storage: sessionStorage
       })
-      console.log(cognitoIdentity.user)
 
-      console.log('new AuthenticationDetails')
       var authnDetails = new AuthenticationDetails({
         Username: username,
         Password: password
       })
 
-      console.log('cognigoUser.authenticateUser')
       cognitoIdentity.user.authenticateUser(authnDetails, {
-        onSuccess: function (result) {
-          // Authn success
-          var accessToken = result.getAccessToken().getJwtToken()
-          cognitoIdentity.token = accessToken
-          // resolve(cognitoIdentity)
+        onSuccess: function (session) {
+          cognitoIdentity.session = session
 
           // credential set
           AWS.config.credentials = new AWS.CognitoIdentityCredentials({
             IdentityPoolId: appConfig.IdentityPoolId,
             Logins: {
-              [loginsKey]: result.getIdToken().getJwtToken()
+              [loginsKey]: session.getIdToken().getJwtToken()
             }
           })
 
-          // refresh
-          AWS.config.credentials.refresh((err) => {
-            if (err) {
-              reject(err)
-            } else {
-              resolve(AWS, cognitoIdentity)
-            }
-          })
+          resolve(cognitoIdentity.user)
         },
         onFailure: function (err) {
           reject(err)
         }
       })
     })
+  },
+  refreshAuth: refreshAuth,
+  logout: function (callback) {
+    sessionStorage.clear()
+    if (callback) {
+      callback()
+    }
   }
 }
