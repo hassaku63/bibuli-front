258598cd2824fee54bd48c0ace6a6a02d905142d
hassaku63 <takuyahashimoto1988@gmail.com>
2019-04-09 21:18:41 +0900

    [feat] books.search

--- src/services/books.js
+++ src/services/books.js
@@ -1,8 +1,40 @@
-// import axios from 'axios'
+import axios from 'axios'
+import Auth from '@/services/auth.js'
+
+axios.defaults.withCredentials = true
+axios.defaults.crossDomain = true
 
 /* eslint-disable no-unused-vars */
+var API_ENDPOINT = 'https://' +
+      process.env.VUE_APP_AWS_API_GATEWAY_ID + '.execute-api.' +
+      process.env.VUE_APP_AWS_API_GATEWAY_REGION + '.amazonaws.com/' +
+      process.env.VUE_APP_AWS_API_GATEWAY_STAGE + '/'
 
 export default {
+  search: function (word, by) {
+    return new Promise((resolve, reject) => {
+      Auth.refreshAuth().then((userSession) => {
+        axios.get(API_ENDPOINT + 'bibuli/search', {
+          headers: {
+            Authorization: userSession.idToken
+          },
+          params: {
+            word, by
+          },
+          data: {}
+        }).then(result => {
+          if ('matched_items' in result.data) {
+            resolve(result.data.matched_items)
+          } else {
+            reject(new Error('no "matched_items" key'))
+          }
+        }, err => {
+          console.log(err)
+          reject(new Error(err))
+        })
+      })
+    })
+  },
   listRentals: function () {
     return new Promise(function (resolve, reject) {
       resolve([
