112a70876ab9df694b64929dde79098b766bfecf
kkato <kazuya.kato@serverworks.co.jp>
2019-05-21 23:22:09 +0900

    cardのデザインを一新しました

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,48 +1,34 @@
 <template>
-<v-flex xs12 sm4>
-  <v-card class="book-card">
-    <v-img v-bind:src="book.thumbnail_Url" aspect-ratio="1.5">
-    <v-chip color="primary" text-color="white" small label>
-      <span>{{ stockChip }}</span>
-    </v-chip>
+  <v-card flat color="transparent">
+    <v-img :src="book.thumbnail_Url" aspect-ratio="0.75">
+      <v-chip :color="stockColor" text-color="white" small label>
+        <span>{{ stockStr }}</span>
+      </v-chip>
     </v-img>
-    <v-card-title>
-      <div>
-        <h3>{{book.title}}</h3>
-        <div>{{book.author}} 著</div>
-        <div>ISBN-{{book.isbn13}}</div>
-      </div>
-    </v-card-title>
+    <v-card-title class="caption px-0 py-1">{{book.title}} | {{book.author}}</v-card-title>
   </v-card>
-</v-flex>
 </template>
 
 <script>
 export default {
   props: ['book'],
 
-  computed: {
-    stockChip: function () {
-      if (this.book.stock <= 0) {
-        return '在庫なし'
-      }
-      return '残り: ' + this.book.stock
+  data () {
+    return {
+      stockStr: '在庫なし',
+      stockColor: '#AAA'
+    }
+  },
+
+  created: function () {
+    if (this.book.stock > 0) {
+      this.stockStr = '残り: ' + this.book.stock
+      this.stockColor = 'primary'
     }
   }
 }
 </script>
 
 <style scoped>
-.book-card {
-  cursor: pointer
-}
-.book-card .v-card__title {
-  position: absolute;
-  height: 60%;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  background-color: rgba(0,0,0,.5);
-  color: #FFF;
-}
+.v-card { cursor: pointer; }
 </style>
